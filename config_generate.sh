da898d755069be78d9d7fda2b9c3f755eba5a72d
Stephen Marks <stephenmarks5309@gmail.com>
2022-01-15 18:49:05 -0800

    Using XDG directory standard, added switches

--- config_generate.sh
+++ config_generate.sh
@@ -7,14 +7,16 @@
 #home partition and SystemD
 #Must manually create user and remount home directory after installation
 
-if [[ -f ./configs ]]; then
-	mkdir ./configs
+if [[ ! -d $XDG_CONFIG_HOME/arch_bootstrap ]]; then
+	mkdir -p $XDG_CONFIG_HOME/arch_bootstrap 
 fi
-pacman -Qe > ./configs/explicitly_installed.txt; pacman -Qm > ./configs/aur_installed.txt
-cp -f /etc/pacman.conf ./configs/pacman.conf.backup 
-cp -f /etc/X11/xorg.conf ./configs/xorg.conf.backup
-cp -rf /usr/share/xsessions ./configs/xsessions.backup
-cp -f /etc/doas.conf ./configs/doas.conf.backup
-cp -f /etc/sudoers ./configs/sudoers.backup
-cp -f /etc/fstab ./configs/fstab.backup
-echo "done"
+pacman -Qe | awk '{print $1}' > $XDG_CONFIG_HOME/arch_bootstrap/explicitly_installed.txt
+pacman -Qm | awk '{print $1}'> $XDG_CONFIG_HOME/arch_bootstrap/aur_installed.txt
+cp -f /etc/pacman.conf $XDG_CONFIG_HOME/arch_bootstrap/pacman.conf.backup 
+cp -f /etc/X11/xorg.conf $XDG_CONFIG_HOME/arch_bootstrap/xorg.conf.backup
+cp -f /etc/doas.conf $XDG_CONFIG_HOME/arch_bootstrap/doas.conf.backup
+cp -f /etc/sudoers $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
+chown steve:steve $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
+grep "swap\|books\|films\|games\|tv" /etc/fstab > \
+$XDG_CONFIG_HOME/arch_bootstrap/fstab.backup 
+echo "Completed Reinstallation Preparation..."
--- post_install.sh
+++ post_install.sh
@@ -5,62 +5,75 @@
 #Description: This is for my own personal use as a reinstallation script 
 #from a previously used Arch based Linux distribution that used a seperate
 #home partition and SystemD
-#Must manually create user and remount home directory after installation
 
+opts=$(getopt -o p:u:h:t --long password:,home:,user:,test -- "$@")
+[ $? -eq 0 ] || { echo "Incorrect options provided"; exit 1}
+eval set -- "$opts"
+unset "$opts"
+while true; do
+	case "$1" in 
+	-p | --password)
+		password={$OPTARG} && make_flag="TRUE";shift;;
+	-u | --user )
+		user_name={$OPTARG};
+		mkdir -p /home/$user_name && export XDG_CONFIG_HOME=/home/$user_name
+		shift;;
+	-h | --home ) 
+		home={$OPTARG} && echo "UUID=$home /home ext4 defaults 0 0" \
+		>> /etc/fstab && mount -a > /dev/null ;shift;;
+	-t | --test )
+		echo "Switches work"
+		exit;;
+	-- )
+		shift
+		break;;
+	* )
+		break;
+	esac
+done
+mk_user() {
+	useradd --badnames --base-dir /home/$user_name -g $user_name --password $password $user_name
+}
 install_explicit() {
 	pacman -Syu
 	echo "Installing all explicit packages"
-	for pkgName in $(/home/steve/scr/arch_bootstrap/configs/explicitly_installed.txt)
+	for pkgName in $(cat $XDG_CONFIG_HOME/arch_bootstrap/explicitly_installed.txt)
 	do
-  	pacman -S --force --noconfirm $pkgName
+  	pacman -S --noconfirm $pkgName
 	done
 	echo "Reinstalled all explicit packages."
 }
 
 install_gits() {
 	echo "Installing all git dependencies"
-	/home/steve/dl/gh_cl/font_stuff/nerd-fonts/install.sh &&
-	( cd /home/steve/dl/gh_cl/yay && makepkg -si ) &&
+	sh /home/$user_name/dl/gh_cl/font_stuff/nerd-fonts/install.sh
+	( cd /home/$user_name/dl/gh_cl/yay && sudo -u $user_name makepkg -si --noconfirm )
 	echo "Installed Git dependencies"
-}
-
-sym_link() {
-	echo "Creating binary sym links"
-	cd /usr/bin &&
-	ln -s /home/steve/dl/gh_cl/suckless/dwm/dwm dwm
-	ln -s /home/steve/dl/gh_cl/suckless/"st-0.8.4"/st st
-	ln -s /home/steve/dl/gh_cl/suckless/dwmblocks-distrotube/dwmblocks
-	ln -s /home/steve/dl/gh_cl/suckless/tabbed/tabbed tabbed
-	ln -s /home/steve/dl/gh_cl/suckless/surf-dsitrotube/surf surf
-	ln -s /home/steve/dl/gh_cl/suckless/dmenu-distrotube/dmenu dmenu
-	echo "Created binary sym links"
 }
 
 install_aur() {
 	echo "Installing aur packages"
-	for pkgName in $(cat /home/steve/scr/arch_bootstrap/aur_installed.txt)
+	for pkgName in $(cat $XDG_CONFIG_HOME/arch_bootstrap/aur_installed.txt)
 	do
-	yay -S --force --noconfirm $pkgName
+	sudo -u $user_name yay -S --force --noconfirm $pkgName
 	done
 	echo "Reinstalled aur packages"
 }
 
 configs() {
-	cp -f ./configs/fstab.backup /etc/fstab
-	cp -f ./configs/doas.conf.backup /etc/doas.conf
-	cp -f ./configs/sudoers.backup /etc/sudoers
-	cp -f ./configs/pacman.conf.backup /etc/pacman.conf
-	cp -f ./configs/xorg.conf.backup /etc/X11/xorg.conf
-	cp -rf ./configs/xsessions.backup /usr/share/xsessions
+	cat $XDG_CONFIG_HOME/arch_bootstrap/fstab.backup >> /etc/fstab && mount -a > /dev/null
+	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/doas.conf.backup /etc/doas.conf
+	chown root:root $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
+	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup /etc/sudoers
+	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/pacman.conf.backup /etc/pacman.conf
+	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/xorg.conf.backup /etc/X11/xorg.conf
 }
 
 services() {
-	systemctl enable sshd ly.service
+	systemctl enable sshd ly.service clamav-freshclam.service cronie.service libvirtd
 }
 
-configs
-install_git
-install_explicit
-install_aur
-sym_link
-services
+sym_links() {
+	ln -s /usr/bin/zsh /usr/bin/sh 
+}
+mk_user;configs;install_explicit;install_gits;install_aur;sym_links;services
