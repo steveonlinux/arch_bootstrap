c9db7b5e5f03a61525838322d5ef18510f02f894
Stephen Marks <stephenmarks5309@gmail.com>
2022-04-22 05:20:21 -0700

    Added more backups, edit mk_user, dash sym_link, added absolute paths

--- config_generate.sh
+++ config_generate.sh
@@ -7,16 +7,21 @@
 #home partition and SystemD
 #Must manually create user and remount home directory after installation
 
-if [[ ! -d $XDG_CONFIG_HOME/arch_bootstrap ]]; then
-	mkdir -p $XDG_CONFIG_HOME/arch_bootstrap 
+if [[ ! -d /home/steve/.config/arch_bootstrap ]]; then
+	mkdir -p /home/steve/.config/arch_bootstrap 
 fi
-pacman -Qe | awk '{print $1}' > $XDG_CONFIG_HOME/arch_bootstrap/explicitly_installed.txt
-pacman -Qm | awk '{print $1}'> $XDG_CONFIG_HOME/arch_bootstrap/aur_installed.txt
-cp -f /etc/pacman.conf $XDG_CONFIG_HOME/arch_bootstrap/pacman.conf.backup 
-cp -f /etc/X11/xorg.conf $XDG_CONFIG_HOME/arch_bootstrap/xorg.conf.backup
-cp -f /etc/doas.conf $XDG_CONFIG_HOME/arch_bootstrap/doas.conf.backup
-cp -f /etc/sudoers $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
-chown steve:steve $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
+pacman -Qe | awk '{print $1}' > /home/steve/.config/arch_bootstrap/explicitly_installed.txt
+pacman -Qm | awk '{print $1}'> /home/steve/.config/arch_bootstrap/aur_installed.txt
+cp -f /etc/pacman.conf /home/steve/.config/arch_bootstrap/pacman.conf.backup 
+cp -f /etc/X11/xorg.conf /home/steve/.config/arch_bootstrap/xorg.conf.backup
+cp -f /etc/doas.conf /home/steve/.config/arch_bootstrap/doas.conf.backup
+cp -f /etc/sudoers /home/steve/.config/arch_bootstrap/sudoers.backup
+chown steve:steve /home/steve/.config/arch_bootstrap/sudoers.backup
+#cp -r /usr/share/xsessions /home/steve/.config/arch_bootstrap/xsessions
+#cp -r /usr/share/wayland-sessions home/steve/.config/arch_bootstrap/wayland-sessions
+cp /etc/mkinitcpio.conf /home/steve/.config/arch_bootstrap/mkinitcpio.conf.backup
+cp /etc/pacman.d/hooks/nvidia.hook /home/steve/.config/arch_bootstrap/nvidia.hook.backup
+cp /etc/default/grub /home/steve/.config/arch_bootstrap/grub.backup
 grep "swap\|books\|films\|games\|tv" /etc/fstab > \
-$XDG_CONFIG_HOME/arch_bootstrap/fstab.backup 
+/home/steve/.config/arch_bootstrap/fstab.backup 
 echo "Completed Reinstallation Preparation..."
--- post_install.sh
+++ post_install.sh
@@ -6,6 +6,8 @@
 #from a previously used Arch based Linux distribution that used a seperate
 #home partition and SystemD
 
+#TO DO: grub mkconfig; copy new backups; 
+
 opts=$(getopt -o p:u:h:t --long password:,home:,user:,test -- "$@")
 [ $? -eq 0 ] || { echo "Incorrect options provided"; exit 1}
 eval set -- "$opts"
@@ -33,12 +35,12 @@ while true; do
 done
 mk_user() {
 	useradd --badnames --base-dir /home/$user_name -g $user_name --password $password $user_name
-	usermod -aG libvirt $user_name
+	usermod -aG wheel,libvirt $user_name
 }
 install_explicit() {
 	pacman -Syu
 	echo "Installing all explicit packages"
-	for pkgName in $(cat $XDG_CONFIG_HOME/arch_bootstrap/explicitly_installed.txt)
+	for pkgName in $(cat /home/steve/.config/arch_bootstrap/explicitly_installed.txt)
 	do
   	pacman -S --noconfirm $pkgName
 	done
@@ -54,7 +56,7 @@ install_gits() {
 
 install_aur() {
 	echo "Installing aur packages"
-	for pkgName in $(cat $XDG_CONFIG_HOME/arch_bootstrap/aur_installed.txt)
+	for pkgName in $(cat /home/steve/.config/arch_bootstrap/aur_installed.txt)
 	do
 	sudo -u $user_name yay -S --force --noconfirm $pkgName
 	done
@@ -62,12 +64,12 @@ install_aur() {
 }
 
 configs() {
-	cat $XDG_CONFIG_HOME/arch_bootstrap/fstab.backup >> /etc/fstab && mount -a > /dev/null
-	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/doas.conf.backup /etc/doas.conf
-	chown root:root $XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup
-	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/sudoers.backup /etc/sudoers
-	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/pacman.conf.backup /etc/pacman.conf
-	cp -f .$XDG_CONFIG_HOME/arch_bootstrap/xorg.conf.backup /etc/X11/xorg.conf
+	cat /home/steve/.config/arch_bootstrap/fstab.backup >> /etc/fstab && mount -a > /dev/null
+	cp -f /home/steve/.config/arch_bootstrap/doas.conf.backup /etc/doas.conf
+	chown root:root /home/steve/.config/arch_bootstrap/sudoers.backup
+	cp -f /home/steve/.config/arch_bootstrap/sudoers.backup /etc/sudoers
+	cp -f /home/steve/.config/arch_bootstrap/pacman.conf.backup /etc/pacman.conf
+	cp -f /home/steve/.config/arch_bootstrap/xorg.conf.backup /etc/X11/xorg.conf
 }
 
 services() {
@@ -75,6 +77,6 @@ services() {
 }
 
 sym_links() {
-	ln -s /usr/bin/zsh /usr/bin/sh 
+	ln -s /usr/bin/dash /usr/bin/sh 
 }
 if [$flag -eq "TRUE"];then mk_user;fi;configs;install_explicit;install_gits;install_aur;sym_links;services
